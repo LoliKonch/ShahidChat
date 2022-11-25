0e2987e393d69cd557460656cbf0a016b7361adb
MbIUIb <kirill.chezlov@gmail.com>
2022-11-25 18:07:02 +0300

    add DARK_VK and DARWIN_TV themes

--- src/main/java/chat/shahid_chat/ColorPalettes.java
+++ src/main/java/chat/shahid_chat/ColorPalettes.java
@@ -45,6 +45,48 @@ public class ColorPalettes {
     };
 
 
+    public static final String[] DARK_VK = new String[]{
+            "#252525", // 0 Main background (registration)
+            "#424242", // 1 Side background (registration)
+            "#397dcc", // 2 Buttons (registration)
+            "#303030", // 3 Fields (registration)
+            "#d6d8db", // 4 Titles (registration)
+            "#d6d8db", // 5 Button text (registration) b4b6b9 36393f
+            "#424242", // 6 Fields borders (registration)
+            "#9c9c9c", // 7 Fields text (registration)
+            "#04a5e6", // 8 Hyperlinks (registration)
+            "", // 9 Your messages
+            "", // 10 Other messages
+            "", // 11 Send buttons
+            "", // 12 Main background
+            "", // 13 Menu background
+            "", // 14 Menu buttons
+            "", // 15 Boarders
+            "", // 16
+    };
+
+
+    public static final String[] DARWIN_TV = new String[]{
+            "#3a3f55", // 0 Main background (registration)
+            "#e4fdff", // 1 Side background (registration)
+            "linear-gradient(to right, #ef7550, #fda76a)", // 2 Buttons (registration)
+            "linear-gradient(to bottom right, #a9c9ba, #B5D4C3)", // 3 Fields (registration)
+            "linear-gradient(to bottom, #b84504, #f9fa56)", // 4 Titles (registration)
+            "#3d423d", // 5 Button text (registration)
+            "#bbbbbb", // 6 Fields borders (registration)
+            "#8a8b91", // 7 Fields text (registration)
+            "#00b4d8", // 8 Hyperlinks (registration)
+            "", // 9 Your messages
+            "", // 10 Other messages
+            "", // 11 Send buttons
+            "", // 12 Main background
+            "", // 13 Menu background
+            "", // 14 Menu buttons
+            "", // 15 Boarders
+            "", // 16
+    };
+
+
     public static final String[] RAINBOW = new String[]{
             "#001845", // 0 Main background (registration)
             "#fafafa", // 1 Side background (registration)
